4a68d7487e470054f1bd0a26aeac38fc27a946dc
Bin Chen <binchen.devops@gmail.com>
2020-02-27 22:36:56 -0500

    finished double pointer section

---
+++ doublepointers/prob234.py
@@ -0,0 +1,46 @@
+# parlindome for singly linked list
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    """time limit exceeded error """    
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        m = self.get_length(head)
+        i = 0
+        curr = head
+        while i < m // 2:
+            mirror = self.get_mirror_node(curr, m, i)
+            if curr.val == mirror.val:
+                i += 1
+                curr = curr.next
+            else:
+                return False
+        return True
+                 
+    def get_length(self, head):
+        m    = 0
+        curr = head
+        while curr:
+            m += 1
+            curr = curr.next
+        return m
+        
+    def get_mirror_node(self, curr, m, i):
+        # given index i of current node , find mirror node
+        if i > m - 1:
+            return None
+        j = m - 2*i - 1
+        node = curr
+        while j > 0:
+            node = node.next
+            j -= 1
+        return node
+               
