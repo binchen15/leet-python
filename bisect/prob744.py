e4072b28fdb01b22a561e2f33f57dccd6922e633
binchen14 <bchen3@fsu.edu>
2020-03-02 12:22:16 -0500

    prob 744 next greatest letter bisect slow version

---
+++ bisect/prob744.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        if target < letters[0] or target >= letters[-1]:
+            return letters[0]
+        
+        l, h = 0, len(letters)-1
+        while l < h:
+            mid = l + (h - l) // 2
+            if letters[mid] <= target:
+                l = mid + 1
+            else:
+                h = mid
+        return letters[l]
+        
