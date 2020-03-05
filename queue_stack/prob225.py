a915144a9a4b76ac1f306d16bb73d15ec6e8ab5f
Bin Chen <binchen.devops@gmail.com>
2020-03-04 21:26:57 -0500

    stack via queue.

---
+++ queue_stack/prob225.py
@@ -0,0 +1,58 @@
+# implementing stack using queue
+class MyStack(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = []
+        
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: None
+        """
+        self.data.append(x)
+        m = len(self.data)
+        if m == 1:
+            return
+        for _ in range(m-1):
+            y = self.pop()
+            self.data.append(y)
+            
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        x = self.data[0]
+        del self.data[0]
+        return x
+        
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        return self.data[0]
+        
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return not bool(self.data)
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
