3e2ad4627a02d88fdc1c9c06076e4f25fd447bf8
binchen14 <bchen3@fsu.edu>
2020-03-06 15:42:32 -0500

    Prob.566 reshape array.

---
+++ arrays_matrices/prob566.py
@@ -0,0 +1,55 @@
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        r0 = len(nums)
+        if r0:
+            c0 = len(nums[0])
+        else:
+            c0 = 0
+        if not r0*c0 or r0*c0 != r*c:
+            return nums
+        
+				#### this array initialization make shallow copy.. mess up
+        row = [0] * c
+        ans = [row] * r
+        
+        for i in range(r):
+            for j in range(c):
+                k = i * c + j
+                i0 = k // c0
+                j0 = k  % c0
+                ans[i][j] = nums[i0][j0]
+        return ans
+
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        r0 = len(nums)
+        if r0:
+            c0 = len(nums[0])
+        else:
+            c0 = 0
+            
+        if not r0*c0 or r0*c0 != r*c:
+            return nums
+        
+        ans = [ [0 for _ in range(c)] for _ in range(r)]
+        for i in range(r):
+            for j in range(c):
+                k = i * c + j
+                i0 = k // c0
+                j0 = k  % c0
+                ans[i][j] = nums[i0][j0]
+        return ans
+        
+        
