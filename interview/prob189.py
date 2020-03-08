3c4fa115bda883cf39b668703b5d3b5b66571b94
Bin Chen <binchen.devops@gmail.com>
2020-03-07 23:21:41 -0500

    prob.189 rotate array

---
+++ interview/prob189.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for _ in range(k):
+            n = nums.pop()
+            nums.insert(0, n)
+            
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        m = len(nums)
+        nums[:] = nums[m-k:] + nums[:m-k]
+        return
