b9ae5dd5fcdc8421e010297e9e4b37ca9a482c20
binchen14 <bchen3@fsu.edu>
2020-03-13 09:50:58 -0400

    prob. 543 diameter of binary tree

---
+++ tree/prob543.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 0
+        
+        dl = self.diameterOfBinaryTree(root.left)
+        dr = self.diameterOfBinaryTree(root.right)
+        d0 = self.height(root.left) + self.height(root.right)
+        return max(dl, dr, d0)
+        
+        
+    def height(self, root):
+        if not root:
+            return 0
+        else:
+            return 1 + max(
+                self.height(root.left),
+                self.height(root.right)
+            )
+        
