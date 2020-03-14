1285aa3cd0a28b9e3bf9bd758acfb7a92174baca
Bin Chen <binchen.devops@gmail.com>
2020-03-13 22:39:04 -0400

    prob 1108 defang ip address

---
+++ strings/prob1108.py
@@ -0,0 +1,9 @@
+# defaing ip address
+
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        return address.replace(".", "[.]")
