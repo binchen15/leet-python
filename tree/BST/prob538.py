a08585aa35c62ab6aef11dc38d3e418c6cbd03d3
Bin Chen <binchen.devops@gmail.com>
2020-03-15 09:55:36 -0400

    prob. 538 BST to Greater Tree

---
+++ tree/BST/prob538.py
@@ -0,0 +1,47 @@
+# 538 BST to Greater Tree
+# 25% solution... slow
+
+class Solution(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return root
+        # return keys as sorted list
+        vals = []
+        self.inorder(root, vals)
+        wc   = {}  # word/value counts
+        for v in vals:
+            wc[v] = wc.get(v, 0) + 1
+            
+        keys = sorted(wc.keys(), reverse=True)
+        
+        sums = {keys[0]:0}  # max key
+        for i in range(1, len(keys)):
+            key  = keys[i]
+            prev = keys[i-1]
+            sums[key] = sums[prev] + prev * wc[prev]  
+        
+        self.greatAgain(root, sums)
+        return root
+    
+            
+    def inorder(self, root, vals):
+        if not root:
+            return
+        if root.left:
+            self.inorder(root.left, vals)
+        vals.append(root.val)
+        if root.right:
+            self.inorder(root.right, vals)
+            
+    def greatAgain(self, root, sums):
+        if not root:
+            return
+        root.val += sums.get(root.val, 0)
+        self.greatAgain(root.left, sums)
+        self.greatAgain(root.right, sums)
+        
+        
