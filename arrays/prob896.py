33697f60799c5914a44f2d7a3b988762087b2a37
Bin Chen <binchen.devops@gmail.com>
2020-03-25 22:40:33 -0400

    prob. 896 Monotonic array

---
+++ arrays/prob896.py
@@ -0,0 +1,27 @@
+# 896 Monotonic array
+
+class Solution(object):
+    def isMonotonic(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+        m = len(A)
+        if m == 1:
+            return True
+        
+        flag = A[1] - A[0]
+        for i in range(2, m):
+            if A[i] > A[i-1]:
+                if flag < 0:
+                    return False
+                elif flag == 0:
+                    flag = A[i] - A[i-1]
+            elif A[i] < A[i-1]:
+                if flag > 0:
+                    return False
+                elif flag == 0:
+                    flag = A[i] - A[i-1]
+        return True
+
+
