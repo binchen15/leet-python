d9771ce5aa9553be8eb6d18c47d5df1ae4715d8d
Bin Chen <binchen.devops@gmail.com>
2020-03-26 22:56:11 -0400

    prob. 62 Unique Paths (Medium)

---
+++ dp/prob62.py
@@ -0,0 +1,35 @@
+# 62 Unique Paths
+
+# used a dp matrix
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [ [1] * n for _ in range(m)]
+        for r in range(1, m):
+            for c in range(1,n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+        return dp[m-1][n-1]
+
+# O(N) space complexity
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        prev = [1] * n
+        curr = [1] * n
+        for r in range(1, m):
+            for c in range(1,n):
+                curr[c] = prev[c] + curr[c-1]
+            prev = curr
+            curr = [1] * n
+        return prev[-1]
+                
+
+
