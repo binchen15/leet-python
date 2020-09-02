877c5d86cf4777656a6a2d099af57c269889f0fb
Bin Chen <binchen.devops@gmail.com>
2020-09-02 09:28:47 -0400

    prob. 700, 701. search and insert into BST

---
+++ tree/BST/prob701.py
@@ -0,0 +1,23 @@
+# 701. Insert into Binary Search Tree
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return TreeNode(val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+
