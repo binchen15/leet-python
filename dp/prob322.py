f17d6ff91a14c515c0943e4fddbd5edc87905206
Bin Chen <binchen.devops@gmail.com>
2021-11-10 11:39:21 -0800

    Prob.322 added top down memo method

--- dp/prob322.py
+++ dp/prob322.py
@@ -18,3 +18,38 @@ class Solution:
                 dp[i] = min(col)
 
         return dp[amount]
+
+# top down recursion with memoization
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        if amount == 0:
+            return 0
+        if amount < 0:
+            return -1
+
+        memo = {}
+
+        def helper(coins, amount):
+
+            if amount == 0:
+                return 0
+            if amount < 0:
+                return -1
+            if amount in memo:
+                return memo[amount]
+
+            candidates = []
+            for c in coins:
+                if amount - c >= 0:
+                    tmp = helper(coins, amount - c)
+                    if tmp != -1:
+                        candidates.append(tmp + 1)
+            if not candidates:
+                ans = -1
+            else:
+                ans = min(candidates)
+            memo[amount] = ans
+            return ans
+
+        return helper(coins,amount)
