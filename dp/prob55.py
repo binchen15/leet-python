c470e817b14b2216c11e938cd3816e4c00ba8bf3
Bin Chen <binchen.devops@gmail.com>
2021-12-10 22:22:33 -0800

    jump game different dp solutionn

--- dp/prob55.py
+++ dp/prob55.py
@@ -20,4 +20,26 @@ class Solution(object):
                     break
         return dp[m-1]
 
+class Solution:
+    
+    def canJump(self, nums: List[int]) -> bool:
+        
+        n = len(nums) 
+        if n == 1:
+            return True
+        
+        dp = [False] * n
+        dp[0] = True
+        for i in range(n):
+            if not dp[i]:
+                continue
+            for delta in range(1, nums[i]+1):
+                if i + delta < n:
+                    dp[i+delta] = True
+                    if i+delta == n-1:
+                        return True
+                else:
+                    break
+            
+        return dp[n-1]
 
