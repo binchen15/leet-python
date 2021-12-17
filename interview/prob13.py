5561783b877fc50f4b9fbc29887a9ee64c35b457
Bin Chen <binchen.devops@gmail.com>
2021-12-16 22:28:22 -0800

    prob13 diff solution using OrderedDict

--- interview/prob13.py
+++ interview/prob13.py
@@ -9,7 +9,7 @@ class Solution(object):
         m = len(s)
         if m == 0:
             return 0
-       
+
         tot = 0
         # IV, IX, XL, XC, CD, CM
         d = {"I":1, "V":5, "X":10, "L":50,
@@ -17,19 +17,46 @@ class Solution(object):
              "IV":4,   "IX":9,
              "XL":40,  "XC":90,
              "CD":400, "CM":900}
-        i = 0 
+        i = 0
         while i < m:
             c = s[i]
             if i < m-1:
                 p = s[i:i+2]  # pair of chars
-                if p in d: 
+                if p in d:
                     tot += d[p]
                     i   += 2
                 else:
-                    tot += d[c] 
+                    tot += d[c]
                     i   += 1
             else:
                 tot += d[c]
                 i   += 1
         return tot
-            
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        from collections import OrderedDict
+        d = OrderedDict()
+        d["IV"] = 4
+        d["IX"] = 9
+        d["XL"] = 40
+        d["XC"] = 90
+        d["CD"] = 400
+        d["CM"] = 900
+
+        d["I"] = 1
+        d["V"] = 5
+        d["X"] = 10
+        d["L"] = 50
+        d["C"] = 100
+        d["D"] = 500
+        d["M"] = 1000
+
+        value = 0
+        for key in d:
+            cnt = s.count(key)
+            value += cnt * d[key]
+            s = s.replace(key,"")
+
+        return value
