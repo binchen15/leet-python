9a97f92e5a2db17f11ea411c910d8b1c9214dd67
Bin Chen <binchen.devops@gmail.com>
2022-01-09 09:18:47 -0800

    prob450 delete node in BST

---
+++ tree/BST/prob450.py
@@ -0,0 +1,90 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        
+        if not root:
+            return root
+        
+        node = self.findNode(root, key)
+        if not node:
+            return root
+        
+        # node is a leaf
+        if not node.left and not node.right:
+            return self.deleteLeafNode(root, node)
+            
+        # swap must be either leaf, or one-legged
+        swap = self.findSwap(node)
+        val = swap.val
+        
+        # swap itself is leaf
+        if not swap.left and not swap.right:
+            node = self.deleteLeafNode(node, swap)
+            #if node:
+            #    node.val = val
+            node.val = val
+        else:
+            self.deleteSwap(node, swap)
+            node.val = val
+        
+        return root
+        
+        
+    def findSwap(self, node):
+        "biggest on the left subtree, or smallest on right subtree"
+        if node.left:
+            cur = node.left
+            while cur.right:
+                cur = cur.right
+        else:
+            cur = node.right
+            while cur.left:
+                cur = cur.left
+        return cur
+        
+        
+    def findNode(self, root, key):
+        "find node with given key"
+        if not root:
+            return None
+        elif root.val == key:
+            ans = root
+        elif root.val > key:
+            ans = self.findNode(root.left, key)
+        else:
+            ans = self.findNode(root.right, key)
+        return ans
+        
+        
+    def deleteLeafNode(self, root, node):
+        "assume node is leaf, delete it"
+        if root == node:
+            return None
+        if root.val > node.val:
+            root.left = self.deleteLeafNode(root.left, node)
+        else:
+            root.right = self.deleteLeafNode(root.right, node)
+        return root
+    
+    
+    def deleteSwap(self, root, node):
+        if root.left == node:
+            if node.left:
+                root.left = node.left
+            else:
+                root.left = node.right
+        elif root.right == node:
+            if node.left:
+                root.right = node.left
+            else:
+                root.right = node.right
+        elif root.val > node.val:
+            self.deleteSwap(root.left, node)
+        else:
+            self.deleteSwap(root.right, node)
