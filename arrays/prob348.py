658f93c09b6f622f411e6098ccc9f92e1d8c961d
Bin Chen <binchen.devops@gmail.com>
2022-01-09 17:00:23 -0800

    prob 348 tictactoe

--- arrays/prob348.py
+++ arrays/prob348.py
@@ -94,3 +94,89 @@ func test_col(i int, val int, board [][]int, n int) bool {
     return true
 }
 
+// solution 2
+type TicTacToe struct {
+    n int
+    board [][]int
+    
+}
+
+
+func Constructor(n int) TicTacToe {
+    board := make([][]int, n)
+    for i := 0; i < n; i++ {
+        board[i] = make([]int, n)
+    }
+    
+    t := TicTacToe {
+        n: n,
+        board: board,
+    }
+    
+    return t
+}
+
+
+func (this *TicTacToe) Move(row int, col int, player int) int {
+    
+    this.board[row][col] = player
+    
+    if this.test_row(row, player) || this.test_col(col, player) {
+       return player 
+    }
+    
+    if row == col && this.test_diag(player) {
+        return player
+    }
+    
+    if row + col == this.n - 1 && this.test_diag2(player) {
+        return player
+    }
+    
+    return 0
+    
+}
+
+func (this *TicTacToe) test_row(row int, player int) bool {
+    for c := 0; c < this.n; c++ {
+        if this.board[row][c] != player {
+            return false
+        }
+    }
+    return true
+}
+
+func (this *TicTacToe) test_col(col int, player int) bool {
+    for r := 0; r < this.n; r++ {
+        if this.board[r][col] != player {
+            return false
+        }
+    }
+    return true
+}
+
+func (this *TicTacToe) test_diag(player int) bool {
+    for i := 0; i < this.n; i++ {
+        if this.board[i][i] != player {
+            return false
+        }
+    }
+    return true
+}
+
+func (this *TicTacToe) test_diag2(player int) bool {
+    n := this.n
+    for i := 0; i < n; i++ {
+        if this.board[i][n-1-i] != player {
+            return false
+        }
+    }
+    return true
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * obj := Constructor(n);
+ * param_1 := obj.Move(row,col,player);
+ */:wq
+
