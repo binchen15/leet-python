6a41f167e2cf5464edc18a68366acb00a8156f40
Bin Chen <binchen.devops@gmail.com>
2022-01-11 17:48:22 -0800

    prob150.go improved

--- interview/prob150.go
+++ interview/prob150.go
@@ -34,3 +34,46 @@ func eval(a int, b int, op string) int {
         return a / b
     }
 }
+
+// faster using switch
+
+import (
+    "strings"
+    "strconv"
+)
+
+func evalRPN(tokens []string) int {
+
+    stack := []int{}
+
+    for _, t := range tokens {
+        if strings.Contains("+-*/", t) {
+            sz := len(stack)
+            a, b := stack[sz-2], stack[sz-1]
+            c := eval(a, b, t)
+            stack = append(stack[:sz-2], c)
+        } else {
+            v, _ := strconv.Atoi(t)
+            stack = append(stack, v)
+        }
+    }
+
+    return stack[0]
+
+}
+
+func eval(a int, b int, op string) int {
+
+    switch op {
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case  "*":
+            return a * b
+        default:
+            return a / b
+    }
+
+}
+
