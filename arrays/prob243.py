e288ff5a426b0f7c7ad8297897f88f8758417e1c
Bin Chen <binchen.swe@gmail.com>
2022-02-08 14:02:16 -0800

    prob243 new solution

--- arrays/prob243.py
+++ arrays/prob243.py
@@ -18,3 +18,21 @@ class Solution:
                     ans = abs(i-j)
                     
         return ans
+class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+                
+        ans = sys.maxsize
+        stack = []
+        
+        for i, w in enumerate(wordsDict):
+            if w in (word1, word2):
+                if not stack:
+                    stack = (i, w)
+                elif stack[1] == w:
+                    stack = (i, w)
+                else:
+                    ans = min(ans, i - stack[0])
+                    stack = (i, w)
+                
+                
+        return ans
