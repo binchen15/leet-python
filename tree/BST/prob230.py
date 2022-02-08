64828c017ec781a93f16d3ca562b1226e96ae7fe
Bin Chen <binchen.devops@gmail.com>
2022-02-08 12:20:05 -0800

    add solution to prob230

--- tree/BST/prob230.py
+++ tree/BST/prob230.py
@@ -20,3 +20,28 @@ class Solution(object):
         ans.append(root.val)
         if root.right:
             self.inorder(root.right, ans)
+
+# same as above. brake if already got k elements
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        data = []
+        self.inorder(root, data, k)
+        return data[k-1]
+        
+    def inorder(self, root, ans, k):
+        # inorder traversal
+        if not root:
+            return 
+        if len(ans) == k:
+            return
+        if root.left:
+            self.inorder(root.left, ans, k)
+        ans.append(root.val)
+        if root.right:
+            self.inorder(root.right, ans, k)
+            
