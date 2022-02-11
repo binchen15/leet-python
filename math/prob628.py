681d04155672dd969431c81960ea8bca124f219e
Bin Chen <binchen.swe@gmail.com>
2022-02-10 22:06:44 -0800

    prob628 maximum product of three numbers

---
+++ math/prob628.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        
+        if n == 3:
+            return nums[0] * nums[1] * nums[2]
+        
+        nums.sort()
+        
+        if nums[0] >= 0:
+            return nums[-1] * nums[-2] * nums[-3]
+        
+        if nums[-1] <= 0:
+            return nums[-1] * nums[-2] * nums[-3]
+        
+        if nums[1] <= 0:
+            return max(nums[-1] * nums[-2] * nums[-3],
+                       nums[-1] * nums[0] * nums[1]
+                      )
+        else:
+            return nums[-1] * nums[-2] * nums[-3]
