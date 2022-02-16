2984ef924e7b58d00e6d8c4b9b71d3313490e819
Bin Chen <binchen.swe@gmail.com>
2022-02-16 11:00:21 -0800

    prob1812 chess board color

---
+++ math/prob1812.py
@@ -0,0 +1,7 @@
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        
+        col = ord(coordinates[0]) - ord('a') + 1 
+        row = int(coordinates[1])
+        
+        return (row + col) % 2 == 1
