6aa98317e7cd99e03803c02f4516c55cdd050fc4
Bin Chen <binchen.swe@gmail.com>
2022-02-16 10:26:16 -0800

    prob.1863 sum of all subsets xor totals

---
+++ backtracking/prob1863.py
@@ -0,0 +1,36 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+
+        flags = []
+        
+        subsets = set()
+        
+        def walk(flags, i):
+            if i == n:
+                indices = []
+                for i in range(n):
+                    if flags[i]:
+                        indices.append(i)
+                subsets.add(tuple(indices))
+                return 
+            
+            flags.append(False)
+            walk(flags, i+1)
+            flags.pop()
+            flags.append(True)
+            walk(flags, i+1)
+            flags.pop()
+            
+        walk(flags, 0)
+        
+        ans = 0
+        
+        for t in subsets:
+            tmp = 0
+            for i in t:
+                tmp ^= nums[i]
+            ans += tmp
+            
+        return ans
