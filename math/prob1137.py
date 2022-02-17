02dd8d979830c66918033c0db922e058e423c5c8
Bin Chen <binchen.swe@gmail.com>
2022-02-17 11:51:28 -0800

    prob1137 N-th Tribonacci Number

---
+++ math/prob1137.py
@@ -0,0 +1,17 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        
+        memo = [0] * (n+1)
+        memo[1], memo[2] = 1, 1
+    
+        for j in range(3, n+1):
+            memo[j] = memo[j-1] + memo[j-2] + memo[j-3]
+            
+        return memo[n]
