527ca78f40f4aae54d0f43ec4519f7a24f1db417
Bin Chen <binchen.swe@gmail.com>
2022-02-18 14:31:05 -0800

    prob263 Ugly number

---
+++ math/prob263.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        
+        if n <= 0:
+            return False
+        
+        while n > 1:
+            if n % 2 == 0:
+                n //= 2
+            elif n % 3 == 0:
+                n //= 3
+            elif n % 5 == 0:
+                n //= 5
+            else:
+                return False
+            
+        return True
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        
+        if n <= 0:
+            return False
+        
+        while n % 2 == 0:
+            n //= 2
+        
+        while n % 3 == 0:
+            n //= 3
+            
+        while n % 5 == 0:
+            n //= 5
+            
+        return n == 1
