4b6619581cef262a8c7bbf9c1fc2f34b5682cdd1
Bin Chen <binchen.swe@gmail.com>
2022-02-18 11:44:22 -0800

    prob342 Power of Four

---
+++ math/prob342.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        
+        if n <= 0:
+            return False
+        
+        while n > 1:
+            n, r = divmod(n, 4)
+            if r != 0:
+                return False
+            
+        return True
