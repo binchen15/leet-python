583a69579e2ea75e108456f911a06e8b21ee3e0a
Bin Chen <binchen.swe@gmail.com>
2022-02-19 18:08:26 -0800

    prob1551 revised

--- math/prob1551.py
+++ math/prob1551.py
@@ -32,3 +32,22 @@ class Solution:
             ops += dops
             dops -= 2
         return ops
+
+class Solution:
+    def minOperations(self, n: int) -> int:
+        
+        if n == 1:
+            return 0
+        
+        l, r = 0, n-1
+        a, b = 2*l+1, 2*r+1
+        dops = r - l 
+        
+        if dops % 2 == 0:
+            m = dops // 2
+            ans = m * (2 + dops) // 2
+        else:
+            m = (dops + 1) // 2
+            ans = m * (1 + dops) // 2
+            
+        return ans
