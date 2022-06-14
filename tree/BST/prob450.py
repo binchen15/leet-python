d451ede935b0390674339c190812a6d4ef351995
Bin Chen <binchen.swe@gmail.com>
2022-06-14 11:10:21 -0700

    prob450 BST remove node

--- tree/BST/prob450.py
+++ tree/BST/prob450.py
@@ -88,3 +88,33 @@ class Solution:
             self.deleteSwap(root.left, node)
         else:
             self.deleteSwap(root.right, node)
+
+#  better solution
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+        if root is None:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+            return root
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+            return root
+        else:  # find the node to delete
+            if root.left is None:
+                return root.right
+            if root.right is None:
+                return root.left
+
+            # root has left & right child
+            successor = root.right
+            while successor.left is not None:
+                successor = successor.left
+
+            root.val = successor.val
+            root.right = self.deleteNode(root.right, successor.val)
+            return root
+
