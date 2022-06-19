e185d8554606faf65cf8af62db98692e993cd3a5
Bin Chen <binchen.swe@gmail.com>
2022-06-18 22:48:26 -0700

    1855 two pointers

--- doublepointers/prob1855.py
+++ doublepointers/prob1855.py
@@ -29,3 +29,20 @@ class Solution:
             else:
                 return 0
 
+#
+
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+
+        n1, n2 = len(nums1), len(nums2)
+        i, j = 0, 0
+        res = 0
+
+        while i < n1 and j < n2:
+            if nums1[i] <= nums2[j]:
+                res = max(res, j-i)
+                j += 1
+            else:
+                i += 1
+
+        return res
