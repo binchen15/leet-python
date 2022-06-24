16d6de8a03288e48c291b9a0c79273aa32e8cdf1
Bin Chen <binchen.swe@gmail.com>
2022-06-23 20:46:43 -0700

    prob1022 Sum of root to leaf binary numbers

---
+++ tree/traversal/prob1022.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+    
+        bag = []
+        def walk(tot, node, bag):
+            if not node:
+                # bag.add(tot)
+                return
+            else:
+                tot = tot * 2 + node.val
+                if not node.left and not node.right:
+                    bag.append(tot)
+                    return
+                if node.left:
+                    walk(tot, node.left, bag)
+                if node.right:
+                    walk(tot, node.right, bag)
+                    
+        walk(0, root, bag)
+        # print(bag)
+        return sum(bag)
