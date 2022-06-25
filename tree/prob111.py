204b9c6479023a8be6d76db7d30a1a9aaeaaa240
Bin Chen <binchen.swe@gmail.com>
2022-06-24 18:15:40 -0700

    prob111 Mini depth of Binary tree BFS method

--- tree/prob111.py
+++ tree/prob111.py
@@ -28,3 +28,26 @@ class Solution(object):
                 return 1 + self.minDepth(root.right)
             else:
                 return 1
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        
+        if not root:
+            return 0
+        
+        cur = [root]
+        nxt = []
+        depth = 1
+        while True:
+            while cur:
+                node = cur.pop(0)
+                if not node.left and not node.right:
+                    return depth
+                if node.left:
+                    nxt.append(node.left)
+                if node.right:
+                    nxt.append(node.right)
+            
+            cur = nxt
+            nxt = []
+            depth += 1
