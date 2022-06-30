9251f33e28dfbefc4a85f2ba7404e116369193c3
Bin Chen <binchen.swe@gmail.com>
2022-06-30 11:07:45 -0700

    1143 LCM add recursion+memo solution

--- dp/prob1143.py
+++ dp/prob1143.py
@@ -10,7 +10,7 @@ def longestCommonSubsequence(self, text1, text2):
         n = len(text2)
         if not m or not n:
             return 0
-        
+
         #dp[i][j] for text1[:i+1] vs text2[:j+1]
         dp = [ [0] * (n+1) for _ in range(m+1) ]
         for i in range(1, m + 1):
@@ -20,5 +20,34 @@ def longestCommonSubsequence(self, text1, text2):
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         return dp[m][n]
-                              
 
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        m = len(text1)
+        n = len(text2)
+
+        if m == 0 or n == 0:
+            return 0
+
+        memo = {}
+
+        def helper(i, j):
+            """text1[i:] vs text2[j:]"""
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if i >= m or j >= n:
+                return 0
+
+            if text1[i] == text2[j]:
+                ans = 1 + helper(i+1, j+1)
+            else:
+                ans = max(helper(i, j+1), helper(i+1, j))
+
+            memo[(i, j)] = ans
+            return ans
+
+        return helper(0, 0)
