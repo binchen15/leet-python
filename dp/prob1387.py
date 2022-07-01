4d1cfe3a932d9d06b86dc72522d5fb1db5d3b783
Bin Chen <binchen.swe@gmail.com>
2022-06-30 23:10:25 -0700

    1387 memoization

--- dp/prob1387.py
+++ dp/prob1387.py
@@ -24,4 +24,32 @@ class Solution:
         numbers = [ Num(v) for v in range(lo, hi+1)]
         numbers.sort()
         return numbers[k-1].val
-                    
+
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+
+        @lru_cache(maxsize=None)
+        def helper(val):
+            if val == 1:
+                return 0
+            if not val & 1:
+                val = val >> 1
+            else:
+                val = 3 * val + 1
+            return 1 + helper(val)
+
+
+        class Num:
+            def __init__(self, val):
+                self.val = val
+                self.power = helper(val)
+
+            def __lt__(self, other):
+                if self.power == other.power:
+                    return self.val < other.val
+                return self.power < other.power
+
+        numbers = [ Num(v) for v in range(lo, hi+1)]
+        numbers.sort()
+        return numbers[k-1].val
+
