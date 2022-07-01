76d94f9300bfab80081847e94195a2df922bff7a
Bin Chen <binchen.swe@gmail.com>
2022-06-30 22:57:33 -0700

    prob1387 Sort Integers by The Power Value

---
+++ dp/prob1387.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        
+        def helper(val):
+            steps = 0
+            while val != 1:
+                if val % 2 == 0:
+                    val //= 2
+                else:
+                    val = 3 * val + 1
+                steps += 1
+            return steps
+        
+        class Num:
+            def __init__(self, val):
+                self.val = val
+                self.power = helper(val)
+                
+            def __lt__(self, other):
+                if self.power == other.power:
+                    return self.val < other.val
+                return self.power < other.power
+            
+        numbers = [ Num(v) for v in range(lo, hi+1)]
+        numbers.sort()
+        return numbers[k-1].val
+                    
