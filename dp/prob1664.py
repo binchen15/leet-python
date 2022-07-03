0b9c629592be33d7d68df36c729cddd09bc01dd8
Bin Chen <binchen.swe@gmail.com>
2022-07-03 12:16:02 -0700

    1664 DP solution

--- dp/prob1664.py
+++ dp/prob1664.py
@@ -21,3 +21,40 @@ class Solution:
             ans += isFair(nums[:i] + nums[i+1:])
 
         return ans
+
+# 2 1-D DP array
+class Solution:
+    def waysToMakeFair(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        if n == 1:
+            return 1
+
+        dp = [0] * n
+        dp[0], dp[1] = nums[0], nums[1]
+        for i in range(2, n):
+            dp[i] = nums[i] + dp[i-2]
+
+        dp2 = [0] * n
+        dp2[-1], dp2[-2] = nums[-1], nums[-2]
+        for i in range(n-3, -1, -1):
+            dp2[i] = nums[i] + dp2[i+2]
+
+        ans = 0
+
+        for i in range(n):
+            # remove nums[i]
+            tmp1, tmp2 = 0, 0
+            if i-1 >= 0:
+                tmp1 += dp[i-1]
+            if i+2 < n:
+                tmp1 += dp2[i+2]
+            if i-2 >= 0:
+                tmp2 += dp[i-2]
+            if i+1< n:
+                tmp2 += dp2[i+1]
+            if tmp1 == tmp2:
+                ans += 1
+
+        return ans
