d9b9517a0f45e7bb2e1e1de98d9531c48e4d20c9
Bin Chen <binchen.swe@gmail.com>
2022-07-04 22:00:28 -0700

    prob44 Wildcard matching DP and Recursion

--- dp/prob44.py
+++ dp/prob44.py
@@ -104,4 +104,118 @@ class Solution(object):
                 dp[0] = False
         return dp[m]
                 
+# timelimit error 1809/1811 cases passed
+class Solution:
 
+    @lru_cache(maxsize=None)
+    def isMatch(self, s: str, p: str) -> bool:
+
+        m = len(s)
+        n = len(p)
+
+        if m == 0:
+            return self.helper(p)
+
+        if n == 0:
+            return False
+
+        if s[0] == p[0]:
+            return self.isMatch(s[1:], p[1:])
+        if p[0] == "?":
+            return self.isMatch(s[1:], p[1:])
+
+        if p[0] == "*":
+            return self.isMatch(s[1:], p) or self.isMatch(s, p[1:])
+
+        return False
+
+
+    @lru_cache(maxsize=None)
+    def helper(self, p):
+        """does p match empty string"""
+
+        # either p is empty, or consists solely of "*"
+
+        if not p:
+            return True
+        s = set(p)
+        if len(s) > 1:
+            return False
+        return "*" in s
+
+# 17% DP solution
+class Solution:
+
+    def isMatch(self, s: str, p: str) -> bool:
+
+        m, n = len(p), len(s)
+
+        dp = [ [0] * (n+1) for _ in range(m+1) ]
+
+        dp[0][0] = 1
+
+        for i in range(1, m+1):
+            dp[i][0] = self.helper(p[:i])
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                # p[:i] vs s[:j]
+                if p[i-1] == s[j-1] or p[i-1] == "?":
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    if p[i-1] != "*":
+                        dp[i][j] = 0
+                    else:
+                        dp[i][j] = dp[i][j-1] or dp[i-1][j]
+
+        return dp[m][n]
+
+
+
+    def helper(self, p):
+        """does p match empty string"""
+
+        # either p is empty, or consists solely of "*"
+        if not p:
+            return 1
+        s = set(p)
+        if len(s) > 1:
+            return 0
+        return 1 if "*" in s else 0
+
+# 50% DP solution
+class Solution:
+
+    def isMatch(self, s: str, p: str) -> bool:
+
+        m, n = len(p), len(s)
+
+        dp = [ [0] * (n+1) for _ in range(m+1) ]
+
+        dp[0][0] = 1
+
+        for i in range(1, m+1):
+            dp[i][0] = self.helper(p[:i])
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                # p[:i] vs s[:j]
+                if p[i-1] == s[j-1] or p[i-1] == "?":
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    if p[i-1] != "*":
+                        dp[i][j] = 0
+                    else:
+                        dp[i][j] = dp[i][j-1] or dp[i-1][j]
+
+        return dp[m][n]
+
+
+
+    def helper(self, p):
+        """does p match empty string"""
+
+        # either p is empty, or consists solely of "*"
+        if not p:
+            return 1
+        return set(p) == set("*")
