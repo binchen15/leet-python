5667a87ecf29fa8eb1febed06bcdb835f9a526c1
Bin Chen <binchen.swe@gmail.com>
2022-07-06 22:57:37 -0700

    Prob140 Work Break II

--- dp/prob140.py
+++ dp/prob140.py
@@ -54,3 +54,30 @@ class Solution:
         walk("", 0)
         
         return ans
+
+# DP solution
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+
+        n = len(s)
+
+        if n == 1:
+            if s in wordDict:
+                return [s]
+            else:
+                return []
+
+        dp = [ [] for _ in range(n+1)]
+        dp[0].append("")
+        for i in range(1, n+1):
+            # s[:i]
+            for word in wordDict:
+                if s[:i].endswith(word):
+                    j = i - len(word)
+                    if j > 0:
+                        for pre in dp[j]:
+                            dp[i].append( pre + " " + word)
+                    else:
+                        dp[i].append(word)
+
+        return dp[n]
