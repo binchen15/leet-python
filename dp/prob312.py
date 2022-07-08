8fac7d5a453ba116c8b6208ce22a062c92dfd3f1
Bin Chen <binchen.swe@gmail.com>
2022-07-08 10:43:08 -0700

    Prob312 Burst Balloons DP + Memoization

--- dp/prob312.py
+++ dp/prob312.py
@@ -39,5 +39,110 @@ class Solution(object):
         self.helper(nums[:m-1], curr+nums[-2]*nums[-1], ans)
         for i in range(1, m-1):
             self.helper(nums[:i]+nums[i+1:], curr+nums[i-1]*nums[i]*nums[i+1], ans)
-            
 
+
+# Jul 8 2022
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        self.ans = 0
+
+        def walk(arr, score):
+            if not arr:
+                self.ans = max(self.ans, score)
+                return
+
+            for i, v in enumerate(arr):
+                tmp = v
+                if i >= 1:
+                    tmp *= arr[i-1]
+                if i < len(arr) - 1:
+                    tmp *= arr[i+1]
+
+                arr2 = arr[:i] + arr[i+1:]
+                walk(arr2, score+tmp)
+
+        walk(nums, 0)
+        return self.ans
+# July 8. one more case passed
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        self.ans = 0
+
+        def walk(arr, score):
+            if not arr:
+                self.ans = max(self.ans, score)
+                return
+
+            if len(arr) == 1:
+                self.ans = max(self.ans, score + arr[0])
+                return
+
+            if len(arr) == 2:
+                self.ans = max(self.ans, score + arr[0]*arr[1] + max(arr[0], arr[1]) )
+                return
+
+            for i, v in enumerate(arr):
+                tmp = v
+                if i >= 1:
+                    tmp *= arr[i-1]
+                if i < len(arr) - 1:
+                    tmp *= arr[i+1]
+
+                arr2 = arr[:i] + arr[i+1:]
+                walk(arr2, score+tmp)
+
+        walk(nums, 0)
+        return self.ans
+
+# worry about the last ballon to burst, instead of first
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        nums = [1] + nums + [1]
+
+        @lru_cache(maxsize=None)
+        def helper(i, j):
+            """nums[i, j+1]"""
+            ans = 0
+            for k in range(i+1, j):
+                ans = max(ans, helper(i, k) + helper(k, j) + nums[i] * nums[k] * nums[j])
+            return ans
+
+        return helper(0, len(nums)-1)
+
+# DP solution. same recursion as above. 60%
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        nums = [1] + nums + [1]
+
+        n = len(nums)
+
+        dp = [ [0] * n for _ in range(n) ]
+
+        for delta in range(2, n):
+            for i in range(n-delta):
+                j = i + delta
+                tmp = 0
+                for k in range(i+1, j):
+                    tmp = max(tmp, dp[i][k] + dp[k][j] + nums[i] * nums[k] * nums[j])
+
+                dp[i][j] = tmp
+
+        return dp[0][n-1]
