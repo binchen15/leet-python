bb80009201f3d7612c487bda2f8783a6c44d917f
Bin Chen <binchen.swe@gmail.com>
2022-07-18 18:15:05 -0700

    prob76. update

--- doublepointers/prob76.py
+++ doublepointers/prob76.py
@@ -35,3 +35,41 @@ class Solution:
                         formed -= 1
 
         return ans
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        m, n = len(s), len(t)
+        if m < n:
+            return ""
+        
+        cnts = {}
+        for c in t:
+            cnts[c] = cnts.get(c, 0) + 1
+            
+        ans = ""
+        size = sys.maxsize
+            
+        valid = 0  # how many valid chars we found 
+        l = 0      # move fast
+        for r in range(m): # move slow
+            c = s[r]
+            if c in cnts:
+                cnts[c] -= 1
+                if cnts[c] == 0:
+                    valid += 1
+                
+                if valid == len(cnts): # this is a valid window
+                    # update the result
+                
+                    while s[l] not in cnts or cnts[s[l]] < 0: # still have balance remained
+                        if s[l] in cnts:
+                            cnts[s[l]] += 1
+                        l += 1
+                    if r-l+1 < size:
+                        size = r-l+1
+                        ans = s[l:r+1]
+                    
+                            
+        return ans
+
