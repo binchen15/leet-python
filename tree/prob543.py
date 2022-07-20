d88667fee6bb9e4ea863f51f81b2c8113052e43f
Bin Chen <binchen.swe@gmail.com>
2022-07-19 17:52:17 -0700

    Prob543 Diameter of Binary Tree. Updated

--- tree/prob543.py
+++ tree/prob543.py
@@ -34,4 +34,44 @@ class Solution(object):
                 self.height(root.left),
                 self.height(root.right)
             )
+
+User function Template for python3
+
+# import sys
+# sys.setrecursionlimit(3000)
+
+from functools import lru_cache
+
+class Solution:
+    
+    #Function to return the diameter of a Binary Tree.
+    def diameter(self, root):
+        # Code here
+        if not root:
+            return 0
+        ans = [0]
         
+        def walk(node):
+            if not node:
+                return
+            if node.left:
+                walk(node.left)
+            if node.right:
+                walk(node.right)
+            ans[0] = max(ans[0], self.helper2(node))
+        
+        walk(root)
+        return ans[0]
+        
+    @lru_cache(maxsize=None)
+    def helper(self, node):
+        # find the depth of a tree
+        if not node:
+            return 0
+        return 1 + max(self.helper(node.left), self.helper(node.right))
+
+    #@lru_cache(maxsize=None)
+    def helper2(self, node):
+        if not node:
+            return 0
+        return 1 + self.helper(node.left) + self.helper(node.right)
