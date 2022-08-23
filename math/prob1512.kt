04641a3b680546a831a819317798e03038ad9cfa
Bin Chen <binchen.swe@gmail.com>
2022-08-22 22:40:53 -0700

    1512 better solution 77%

--- math/prob1512.kt
+++ math/prob1512.kt
@@ -12,3 +12,20 @@ class Solution {
         return ans
     }
 }
+
+// map based solution
+class Solution {
+    fun numIdenticalPairs(nums: IntArray): Int {
+        var ans = 0
+        val map: MutableMap<Int, Int> = mutableMapOf()
+        for (v in nums) {
+            map.put(v, map.getOrDefault(v, 0) + 1 )
+        }
+        for ((k, v) in map){
+            if (v > 1) {
+                ans += v*(v-1)/2
+            }
+        }
+        return ans
+    }
+}
